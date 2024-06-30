3210ace0fd4f0fc2613353cbde0a2ee7635b030c
koldun <eg.sharypov@ya.ru>
2024-06-30 10:33:05 +0300

    filter panel styling

--- src/features/search/ui/SearchParams/SearchParams.tsx
+++ src/features/search/ui/SearchParams/SearchParams.tsx
@@ -2,6 +2,7 @@ import Dropdown from "../../../../components/Dropdown/Dropdown";
 import { GENRES_MAP, GenresEnglish, YEARS } from "../../../../models/movie";
 import { useAppDispatch, useAppSelector } from "../../../../store";
 import { selectQuery, setSearchParam } from "../../searchSlice";
+import classes from "./SearchParams.module.css";
 
 const genreOptions: {
   text: string;
@@ -22,19 +23,24 @@ export default function SearchParams() {
   const { genre, release_year } = useAppSelector(selectQuery);
   const dispatch = useAppDispatch();
   return (
-    <div>
-      Жанр
-      <Dropdown
-        options={genreOptions}
-        select={(genre) => dispatch(setSearchParam(["genre", genre]))}
-        selected={genre}
-      />
-      Год
-      <Dropdown
-        options={yearOptions}
-        select={(year) => dispatch(setSearchParam(["release_year", year]))}
-        selected={release_year}
-      />
+    <div className={classes.panel}>
+      <div className={classes.title}>Фильтр</div>
+      <div>
+        <div className={classes.label}>Жанр</div>
+        <Dropdown
+          options={genreOptions}
+          select={(genre) => dispatch(setSearchParam(["genre", genre]))}
+          selected={genre}
+        />
+      </div>
+      <div>
+        <div className={classes.label}>Год</div>
+        <Dropdown
+          options={yearOptions}
+          select={(year) => dispatch(setSearchParam(["release_year", year]))}
+          selected={release_year}
+        />
+      </div>
     </div>
   );
 }
--- src/pages/FeedPage/FeedPage.tsx
+++ src/pages/FeedPage/FeedPage.tsx
@@ -9,8 +9,10 @@ export default function FeedPage() {
     <>
       <Header />
       <main className={classes.feedPage}>
-        <SearchParams />
-        <div>
+        <div className={classes.col1}>
+          <SearchParams />
+        </div>
+        <div className={classes.col2}>
           <SearchInput />
           <MovieList />
         </div>
