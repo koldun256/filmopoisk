5ab7667dd1c70ef939c73ec9b8f31a4b36eba90b
koldun <eg.sharypov@ya.ru>
2024-06-30 15:33:05 +0300

    added stars to detail page

--- src/pages/MovieDetailsPage/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -2,9 +2,14 @@ import { useParams } from "react-router-dom";
 import { api } from "../../services/api";
 import Header from "../../components/Header/Header";
 import classes from "./MovieDetailsPage.module.css";
+import { useAppSelector } from "../../store";
+import { selectLoggedIn } from "../../features/auth/authSlice";
+import ScoreBar from "../../features/score/ui/ScoreBar/ScoreBar";
 
 export default function MovieDetailsPage() {
   const { id } = useParams();
+  if (!id) return <div>No movie selected</div>;
+  const loggedIn = useAppSelector(selectLoggedIn);
   const { data: movie, error, isLoading } = api.useGetMovieByIdQuery(id!);
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
@@ -26,6 +31,11 @@ export default function MovieDetailsPage() {
           </p>
           <p>{movie!.description}</p>
         </div>
+        {loggedIn && (
+          <div className={classes.scoreContainer}>
+            <ScoreBar movieId={parseInt(id!)} />
+          </div>
+        )}
       </div>
       <h1 style={{ marginLeft: 24 }}>Актеры</h1>
       <div className={classes.actors}>
