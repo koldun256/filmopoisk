b4dd60b396669d6bfd439e1cca5b2f8104510ee6
koldun <eg.sharypov@ya.ru>
2024-06-30 12:04:30 +0300

    basic modal window styling

--- src/features/auth/ui/LoginDialog/LoginDialog.tsx
+++ src/features/auth/ui/LoginDialog/LoginDialog.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../../store";
 import { LoginStatus, login, selectStatus } from "../../authSlice";
 import ReactDOM from "react-dom";
+import classes from "./LoginDialog.module.css";
 
 type Props = {
   open: boolean;
@@ -20,13 +21,42 @@ export default function LoginDialog({ open, close }: Props) {
 
   if (status == LoginStatus.LoggedIn) close();
   return ReactDOM.createPortal(
-    <dialog open={open}>
-      <input onChange={(e) => setUsername(e.target.value)} value={username} />
-      <input onChange={(e) => setPassword(e.target.value)} value={password} />
-      <button onClick={onSubmit}>Войти</button>
-      <button onClick={close}>Закрыть</button>
-      {status == LoginStatus.Failed && <div>Неправильный логин или пароль</div>}
-      {status == LoginStatus.Pending && <div>Ожидание...</div>}
+    <dialog open={open} className={classes.tint}>
+      <div className={classes.window}>
+        <h3>Авторизация</h3>
+        <div>
+          <label>
+            Логин <span style={{ color: "red" }}>*</span>
+          </label>
+          <br />
+          <input
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+        </div>
+        <div>
+          <label>
+            Пароль <span style={{ color: "red" }}>*</span>
+          </label>
+          <br />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        {status == LoginStatus.Failed && (
+          <div>Неправильный логин или пароль</div>
+        )}
+        {status == LoginStatus.Pending && <div>Ожидание...</div>}
+        <div>
+          <button onClick={onSubmit} className={classes.submit}>
+            Войти
+          </button>
+          <button onClick={close} className={classes.cancel}>
+            Отменить
+          </button>
+        </div>
+      </div>
     </dialog>,
     document.body
   );
