d9886a7add2597ff2a60d104e8ea4f06f0cff90d
koldun <eg.sharypov@ya.ru>
2024-06-30 09:51:26 +0300

    header styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 import classes from "./Header.module.css";
 import LoginButton from "../../features/auth/ui/LoginButton/LoginButton";
+import { useAppSelector } from "../../store";
+import { selectLoggedIn } from "../../features/auth/authSlice";
+import person from "./person.svg";
 
 export default function Header() {
+  const loggedIn = useAppSelector(selectLoggedIn);
   return (
     <header className={classes.header}>
       <Link to="/" className={classes.title}>
-        ФИЛЬМОПОИСК
+        Фильмопоиск
       </Link>
+      {loggedIn && (
+        <div className={classes.ava}>
+          <img src={person}></img>
+        </div>
+      )}
       <LoginButton />
     </header>
   );
--- src/features/auth/ui/LoginButton/LoginButton.tsx
+++ src/features/auth/ui/LoginButton/LoginButton.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../../store";
 import { logOut, selectLoggedIn } from "../../authSlice";
 import LoginDialog from "../LoginDialog/LoginDialog";
-
+import classes from "./LoginButton.module.css";
 export default function LoginButton() {
   const loggedIn = useAppSelector(selectLoggedIn);
   const dispatch = useAppDispatch();
@@ -11,9 +11,16 @@ export default function LoginButton() {
     <>
       <LoginDialog open={open} close={() => setOpen(false)} />
       {loggedIn ? (
-        <button onClick={() => dispatch(logOut())}>Выйти</button>
+        <button
+          onClick={() => dispatch(logOut())}
+          className={`${classes.button} ${classes.logout}`}
+        >
+          Выйти
+        </button>
       ) : (
-        <button onClick={() => setOpen(true)}>Войти</button>
+        <button onClick={() => setOpen(true)} className={classes.button}>
+          Войти
+        </button>
       )}
     </>
   );
