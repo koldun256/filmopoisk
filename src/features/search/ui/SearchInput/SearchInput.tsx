b665dcf4cfcff83acdd9dcf449eb1e7ca350371f
koldun <eg.sharypov@ya.ru>
2024-06-30 11:24:40 +0300

    search input styling

--- src/features/search/ui/SearchInput/SearchInput.tsx
+++ src/features/search/ui/SearchInput/SearchInput.tsx
@@ -1,11 +1,14 @@
 import { useDispatch } from "react-redux";
 import { setSearchParam } from "../../searchSlice";
+import classes from "./SearchInput.module.css";
 
 export default function SearchInput() {
   const dispatch = useDispatch();
   return (
     <input
+      className={classes.input}
       onChange={(e) => dispatch(setSearchParam(["title", e.target.value]))}
+      placeholder="Название фильма"
     />
   );
 }
